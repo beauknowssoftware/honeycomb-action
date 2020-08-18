9a9468339bd6db28d435adb5af971df346997f67
Beau Bennett <beau.bennett@beauknowssoftware.com>
2020-08-18 19:21:21 -0400

    Error on missing required inputs

--- index.js
+++ index.js
@@ -2,10 +2,24 @@ const core = require('@actions/core')
 const fetch = require('node-fetch')
 
 async function run() {
+  // required
   const xHoneycombTeam = core.getInput('X-Honeycomb-Team')
   const dataset = core.getInput('dataset')
   const type = core.getInput('type')
   const message = core.getInput('message')
+  if ( !xHoneycombTeam ) {
+    throw new Error("missing X-Honeycomb-Team input")
+  }
+  if ( !dataset ) {
+    throw new Error("missing dataset input")
+  }
+  if ( !type ) {
+    throw new Error("missing type input")
+  }
+  if ( !message ) {
+    throw new Error("missing message input")
+  }
+
   const url = core.getInput('url')
 
   const res = await fetch(`https://api.honeycomb.io/1/markers/${dataset}`, {
